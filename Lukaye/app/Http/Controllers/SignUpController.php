ccd0a47141eaac35267e40f9d2bd956cf857dd4a
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-11-16 19:06:09 +0100

    add a new of paginate

--- Lukaye/app/Http/Controllers/SignUpController.php
+++ Lukaye/app/Http/Controllers/SignUpController.php
@@ -35,8 +35,9 @@ class SignUpController extends Controller
         return view("users.login");
     }
     public function listUsers(){
-        $clients = client::paginate(2);
-        return view("users.listUsers", ['client' => $clients]);
+        // $clients = client::paginate(6);
+        return view("users.listUsers", client::paginate(6));
+        // return view("users.listUsers", ['client' => $clients]);
         //ce code signifie que je récupère toutes les données de la table client et je les passe dans la vue listUsers
     }
     public function connexion(){

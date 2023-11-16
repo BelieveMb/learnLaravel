071b5e27515e7bbba811ebcca1b7ee120c6b9491
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-11-16 18:34:01 +0100

    je récupère les données de la table etudian

--- Lukaye/app/Http/Controllers/SignUpController.php
+++ Lukaye/app/Http/Controllers/SignUpController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\etudiant;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -33,8 +34,9 @@ class SignUpController extends Controller
         return view("users.login");
     }
     public function listUsers(){
-        
-        return view("users.listUsers");
+        $etudiant = etudiant::all();
+        return view("users.listUsers", ['etudiant' => $etudiant]);
+        //ce code signifie que je récupère toutes les données de la table etudiant et je les passe dans la vue listUsers
     }
     public function connexion(){
         return view("users.connexion");
---
+++ Lukaye/app/Models/etudiant.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class etudiant extends Model
+{
+    use HasFactory;
+    protected $table = 'etudiant';
+    //préciser la table de sélection
+}

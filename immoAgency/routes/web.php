a6b60474f562f06f71dafdd5839992128b781172
Dev Unbelievable <mbolokobelieve@gmail.com>
2024-01-31 16:46:59 +0100

    test chatity are done

--- immoAgency/routes/web.php
+++ immoAgency/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\PropertyController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -9,30 +9,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
-
-use App\Events\Message;
-use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::post('send-message', function (Request $request) {
-Route::post('send-message', function (Request $request) {
-    event(new Message($request->username, $request->message));
-    return ['success' => true];
-});
-Route::prefix('admin')->name('admin.')->group(function(){
-    //prefix permet de mettre un prefix sur toutes les routes, name : le nom de
-    //la routes
-    // Route::resource('proprety', PropertyController::class);
-    //ressource pour générer auto, les routes ->php artisan route:list
-    Route::resource('property', PropertyController::class)->except(['show']);
-    //except, les méthodes à supprimer
-    // Cette route crée plusieurs routes pour le contrôleur PropertyController en utilisant la méthode resource() de Laravel. Cette méthode crée automatiquement des routes pour les actions courantes de CRUD (Create, Read, Update, Delete) associées à un contrôleur.
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
